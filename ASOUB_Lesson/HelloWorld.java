cbe9731c52d60fc58e168f801d6c11370f78eeb4
mr.khangaikhuu uvgunkhuu <mr.khangaikhuu@asu.local>
2018-09-06 08:01:56 +0800

    merged wednesday assignment

---
+++ ASOUB_Lesson/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * My first Hello World Java Program
+ *
+ * @author (Khangaikhuu)
+ * @version (1.0)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
