c38037ca525eeb8ffd5ef008c0363eebefc97a9b
G8 <G8@asu.local>
2018-10-12 09:11:25 +0800

    changed key up down

---
+++ ASOUB_Lesson/Apple.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Apple here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Apple
+{
+    public class Container
+    {
+        int[]  cont1 = {9,5,7,8,4};
+    }
+}
