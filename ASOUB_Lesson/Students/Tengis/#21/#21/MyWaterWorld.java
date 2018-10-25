f3bb3e633afda438d76ec0d8f70ec73aa09509ba
G8 <G8@asu.local>
2018-10-25 09:54:11 +0800

    #21

--- ASOUB_Lesson/Students/Tengis/#21/#21/MyWaterWorld.java
+++ ASOUB_Lesson/Students/Tengis/#21/#21/MyWaterWorld.java
@@ -7,7 +7,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class MyWaterWorld
- extends World
+extends World
 {
 
     /**
@@ -15,9 +15,18 @@ public class MyWaterWorld
      * 
      */
     public MyWaterWorld
-   ()
+    ()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
