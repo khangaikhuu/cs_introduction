291e7655ec0e2229de86c7ad03209c3fc5bf2dec
G8 <G8@asu.local>
2018-10-26 09:47:04 +0800

    Merging

---
+++ ASOUB_Lesson/Students/Khuvituguldur/HappyFriday/BasketballGame/GoldenBall.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GoldenBall here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GoldenBall extends Actor
+{
+    int dx = 11;
+    int dy = 5;
+    double e = 2.779;
+    public void act() 
+    {
+        setLocation(0, 500)
+        seLocation(getX) = dx, getY() + getypos(dx)); 
+    } 
+    public double getypos(int x)
+    {
+          return getypos;
+    }
+}
