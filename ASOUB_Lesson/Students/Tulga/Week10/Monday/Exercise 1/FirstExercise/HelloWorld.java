cb5f96cf3cb095e093e19f5a3e1085bab585cea1
G9 <g9@asu.local>
2018-10-29 09:13:08 +0800

    exercise 1 week 10

---
+++ ASOUB_Lesson/Students/Tulga/Week10/Monday/Exercise 1/FirstExercise/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public void printHelloWorld(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
