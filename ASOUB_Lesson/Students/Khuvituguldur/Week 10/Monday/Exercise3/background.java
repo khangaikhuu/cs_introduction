daa2e3d8b565e3c1b144e617b48986dfdd35c623
G8 <G8@asu.local>
2018-10-29 09:43:32 +0800

    added exercise 3

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 10/Monday/Exercise3/background.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class background extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class background.
+     * 
+     */
+    public background()
+    {
+        
+    }
+}
