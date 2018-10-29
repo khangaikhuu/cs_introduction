75d04a558b2bb41c20ac245d5c8fdc4cbf19662d
G8 <G8@asu.local>
2018-10-29 09:23:18 +0800

    adding class that multiplies 20 10 and 4

---
+++ ASOUB_Lesson/Students/Tugsbilguun/Monday/Monday/Multiply.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Multiply here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Multiply
+{
+  private int x=20;
+  private int y=10;
+  private int z=4;
+  public int multiplyNumber(){
+      return x*y*z;
+  }
+}
