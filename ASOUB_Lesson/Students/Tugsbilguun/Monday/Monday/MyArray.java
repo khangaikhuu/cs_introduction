9057fe4a1d96817a181a5d183fe7f03aa383389c
G8 <G8@asu.local>
2018-10-29 08:57:14 +0800

    #26 adding exercise 3

---
+++ ASOUB_Lesson/Students/Tugsbilguun/Monday/Monday/MyArray.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class MyArray here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyArray
+{
+    private int[] intarray = {1,2,3,4};
+    public int getArraySize(){
+        return intarray.length;
+    }
+}
