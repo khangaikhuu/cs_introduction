ca31a343b8c58f4f7dd3c210af22178cd2e80990
G10 <g10@asu.local>
2018-10-29 09:25:31 +0800

    added exercise 3

---
+++ ASOUB_Lesson/Students/Manal/Week 10/Monday/Exercise 3/Project3.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Project3 here.
+ *
+ * @author (Manal)
+ * @version (Week 10 Monday)
+ */
+public class Project3
+{
+    public void swapNum(int a, int b){
+        int c = a;
+        System.out.println("Original a = " + a);
+        System.out.println("Original b = " + b);
+        a = b;
+        b = c;
+        System.out.println("Swapped a = " + a);
+        System.out.println("Swapped b = " + b);
+    }
+}
