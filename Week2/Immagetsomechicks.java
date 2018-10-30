1cd4882e782f2a015573fb19cebd7f3e92ca5e74
anandokrrr <anandbayarbat@gmail.com>
2018-10-30 16:04:29 +0800

    nothing

---
+++ Week2/Immagetsomechicks.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Immagetsomechicks here.
+ *
+ * @author (whaha)
+ * @version (fridayshdee)
+ */
+public class Immagetsomechicks
+{
+       private String myConstant = "Hello World!!!";
+    private String helloWorld = "Hello World!!!";
+    
+    
+   public int minusFive ( int a )
+   {
+       return a+5;
+   }
+   public double multiply ( double a, double b )
+   {
+       return a*b;
+   }
+   
+}
---
+++ Week2/MyClass.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class MyClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyClass
+{
+    private int myVariable;
+    
+    private int myMethod ()
+    {
+        return 5;
+    }
+}
