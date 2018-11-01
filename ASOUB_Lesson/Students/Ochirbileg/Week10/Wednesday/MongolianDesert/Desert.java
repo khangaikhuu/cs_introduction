e5e9e8b6b8758ded08a55303db8af64d2e4bd5f5
G8 <G8@asu.local>
2018-11-01 08:24:53 +0800

    added MongolianDesert

--- ASOUB_Lesson/Students/Ochirbileg/Week10/Wednesday/MongolianDesert/Desert.java
+++ ASOUB_Lesson/Students/Ochirbileg/Week10/Wednesday/MongolianDesert/Desert.java
@@ -17,5 +17,7 @@ public class Desert extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(500, 600, 1); 
+        Camel camel = new Camel();
+        addObject (camel,100,200);
     }
 }
