83860813dc1549261b4fe75bfd58a6f7be1d2be2
G8 <G8@asu.local>
2018-11-01 08:20:53 +0800

    added tic tac toe

---
+++ ASOUB_Lesson/Students/Ochirbileg/Week10/Monday/Exercise 3/Snake/SnakeWorld.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Snake lives in a world of 20 by 20 squares.
+ * 
+ * @author (Antosh) 
+ * @version (Week10)
+ */
+public class SnakeWorld extends World
+{
+
+    public SnakeWorld()
+    {    
+        super(30, 20, 20); //super(600,400,1);
+        GreenfootImage img = new GreenfootImage(20,20);
+        img.drawRect(0,0,19,19);
+        setBackground(img);
+    }
+}
