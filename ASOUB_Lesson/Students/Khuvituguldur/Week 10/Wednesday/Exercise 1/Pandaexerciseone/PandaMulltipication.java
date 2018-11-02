3191a23a00f22d8ff0515e0eabbf63f629ede195
G8 <G8@asu.local>
2018-11-02 08:18:16 +0800

    added classwork

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 10/Wednesday/Exercise 1/Pandaexerciseone/PandaMulltipication.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class PandaMulltipication here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PandaMulltipication
+{
+  private int[] panda = {1, 2, 3, 4, 5, 6 ,7 , 8, 9, 10};
+  public void PrintsPanda()
+  {
+       for(int i = 0; i < panda.length; i++)
+       {   
+           System.out.println(panda[i]);
+       }
+       
+  }
+}
---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 10/Wednesday/Exercise 3/PandaNumberThree/Bear.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bear here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bear extends Actor
+{
+    /**
+     * Act - do whatever the Bear wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        setLocation(300, 400);
+        setLocation(320, 400);
+        setLocation(350,400);
+        setLocation(400, 400);
+        setLocation(450, 400);
+    }    
+}
