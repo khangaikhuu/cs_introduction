d7a9eb5eafaeaa280af867130ead202a86d48431
Tuguldur1122 <mhuvituguldur@gmail.com>
2018-11-04 14:10:09 -0800

    added Mr.Khangaikhuu

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Let's rock with Greenfoot/RockingRoll/Ocean.java
@@ -0,0 +1,18 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Ocean extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class Ocean.
+     */
+    public Ocean()
+    {
+    }
+}
