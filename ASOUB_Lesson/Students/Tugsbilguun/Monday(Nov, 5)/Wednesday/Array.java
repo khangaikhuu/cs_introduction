97b3450be6d948bf61a3c4b484530cda8535907c
G8 <G8@asu.local>
2018-11-05 09:17:09 +0800

    returns the biggest value of array

--- ASOUB_Lesson/Students/Tugsbilguun/Monday(Nov, 5)/Wednesday/Array.java
+++ ASOUB_Lesson/Students/Tugsbilguun/Monday(Nov, 5)/Wednesday/Array.java
@@ -9,4 +9,13 @@ public class Array
 {
     private int[] intarray={3, 4,5,6,1,15,7,3,23};
     public int biggestNumOfArray()
+    {
+        int max=0;
+        for (int i = 0; i<intarray.length; i++){
+            if(max<=intarray [i]){
+                max=intarray[i];
+            }
+        }
+        return max;
+    }
 }
