d780a9108f7391cf535863656b5428131eab059a
G8 <G8@asu.local>
2018-11-05 09:39:55 +0800

    added manal

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 11/PandaChess/Chess/MyWorld.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(200, 200, 1);
+    }
+}
