a4a5b59c8ef76e706b7900236a50fd0318db1ab5
G10 <g10@asu.local>
2018-11-06 09:35:20 +0800

    #65 adding multiplication

--- ASOUB_Lesson/Students/Namuun/week 11/Tuesday/November6/Multiplication.java
+++ ASOUB_Lesson/Students/Namuun/week 11/Tuesday/November6/Multiplication.java
@@ -2,15 +2,19 @@
 /**
  * Write a description of class Multiplication here.
  *
- * @author (Namuun)
+ * @author ()
  * @version (November 6 Week  Tuesday)
  */
 public class Multiplication
 {
     int [] array = {2, 3, 1, 4, 5, 1, 10, 8, 9, 23, 34};
-
-    public 
+    int multiplication = 1;
+    public void product()
     {
-        
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            multiplication = multiplication * array[i];
+        }
+        System.out.println (multiplication);
     }
 }
