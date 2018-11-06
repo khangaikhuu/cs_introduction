54a02f73ebf10b0d3472ba5555d5dce0d4dc99bf
G9 <g9@asu.local>
2018-11-06 09:47:51 +0800

    #66 Added Basketball game

---
+++ ASOUB_Lesson/Students/Tulga/Week11/Tuesday/ChicagovsCavs/CediOsman.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class CediOsman here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CediOsman extends Actor
+{
+    /**
+     * Act - do whatever the CediOsman wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
