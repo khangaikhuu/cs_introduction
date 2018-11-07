241535eae1c444c3ca71852abd061c009ef138e7
G9 <g9@asu.local>
2018-11-07 09:25:26 +0800

    #78 CalculateRadius

---
+++ ASOUB_Lesson/Students/Tulga/Week11/Wednesday/array/CalculateArea.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class CalculateArea here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CalculateArea
+{
+    private double pi = 3.14;
+    public double CalculateArea ( int radius ) 
+    {
+        return pi * 2 * radius;
+    }
+}
