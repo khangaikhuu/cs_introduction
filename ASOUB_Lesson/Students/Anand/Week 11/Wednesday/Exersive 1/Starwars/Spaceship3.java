218f905ea1f6322a170552803d828fb8ca1f4c29
G8 <G8@asu.local>
2018-11-07 10:07:00 +0800

    #83

---
+++ ASOUB_Lesson/Students/Anand/Week 11/Wednesday/Exersive 1/Starwars/Spaceship3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spaceship3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spaceship3 extends Actor
+{
+    /**
+     * Act - do whatever the Spaceship3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
