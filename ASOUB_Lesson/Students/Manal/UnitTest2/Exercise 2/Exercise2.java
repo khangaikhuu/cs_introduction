8ee9b63bbdbce878ccf247e1c83a836dbec8807f
G10 <g10@asu.local>
2018-11-12 09:33:14 +0800

    unittest2 all

---
+++ ASOUB_Lesson/Students/Manal/UnitTest2/Exercise 2/Exercise2.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Exercise1 here.
+ *
+ * @author (Manal)
+ * @version (a version number or a date)
+ */
+public class Exercise2
+{
+    public void concant(String a, String b){
+        System.out.println(a + b);
+    }
+}
