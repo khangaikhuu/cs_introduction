a9c3afa8b20640a90dc08198f0416d83439ef90d
G8 <G8@asu.local>
2018-11-12 09:17:42 +0800

    PerimeterOfTheCircle added

---
+++ ASOUB_Lesson/Students/Ochirbileg/Unittest2/Exercise 1/Exercise1/PerimeterOfTheCircle.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class PerimeterOfTheCircle here.
+ *
+ * @author (Antosh)
+ * @version (Week12)
+ */
+public class PerimeterOfTheCircle
+{
+   private static final double radius = 7.5;
+   public static void main(String[] args) 
+   {
+        double perimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter is = " + perimeter);
+   }
+}
