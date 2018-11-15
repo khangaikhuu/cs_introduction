d6fa802bf20b09390cbd9a74cf7089662697ec0a
mr.khangaikhuu uvgunkhuu <mr.khangaikhuu@asu.local>
2018-11-15 09:44:38 +0800

    merging

---
+++ ASOUB_Lesson/Students/Ochirbileg/Week12/Exercise 2/Exercise2/LargestElementArray.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class LargestElementArray here.
+ *
+ * @author (Antosh)
+ * @version (Week12)
+ */
+public class LargestElementArray
+{
+     static int arr[] = {10, 324, 45, 90, 9808}; 
+     static int largest() 
+     { 
+         int i; 
+         int max = arr[0]; 
+         for (i = 1; i < arr.length; i++) 
+             if (arr[i] > max) 
+                 max = arr[i]; 
+        
+         return max; 
+     } 
+     public static void main(String[] args)  
+     { 
+         System.out.println("Largest in given array is " + largest()); 
+        } 
+}
