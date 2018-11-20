ecfa4ff691a30cc1dd8ba7d998c532d4dfbb138b
G8 <G8@asu.local>
2018-11-20 09:20:37 +0800

    added graphic card

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 14/GraphicCard/Memory.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Memory here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Memory extends CPU
+{
+    /**
+     * Act - do whatever the Memory wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
