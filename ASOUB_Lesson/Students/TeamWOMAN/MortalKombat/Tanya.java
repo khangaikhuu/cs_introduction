24a7ebcfc2ca34ca63090d2c49af24f40d13db89
G8 <G8@asu.local>
2018-11-29 09:28:09 +0800

    edits to mortal kombat

--- ASOUB_Lesson/Students/TeamWOMAN/MortalKombat/Tanya.java
+++ ASOUB_Lesson/Students/TeamWOMAN/MortalKombat/Tanya.java
@@ -1,19 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Goku here.
+ * Write a description of class Tanya here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Goku extends Actor
+public class Tanya extends Actor
 {
     /**
-     * Act - do whatever the Goku wants to do. This method is called whenever
+     * Act - do whatever the Tanya wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
         // Add your action code here.
+        GreenfootImage image = getImage();
+        image.scale(100,150);
+        setImage(image);
     }    
 }
--- ASOUB_Lesson/Students/TeamWOMAN/MortalKombat/vegeta.java
+++ ASOUB_Lesson/Students/TeamWOMAN/MortalKombat/vegeta.java
@@ -15,5 +15,8 @@ public class vegeta extends Actor
     public void act() 
     {
         // Add your action code here.
+        GreenfootImage image = getImage();
+        image.scale(100,150);
+        setImage(image);
     }    
 }
