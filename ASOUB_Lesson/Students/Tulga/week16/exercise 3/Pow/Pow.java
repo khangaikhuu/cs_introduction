0e5794ab396ab5935d88f8d47265012332bd348a
G9 <g9@asu.local>
2018-12-03 09:28:35 +0800

    #176 Pow

---
+++ ASOUB_Lesson/Students/Tulga/week16/exercise 3/Pow/Pow.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Pow here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pow
+{
+    public int calculatePow (int a, int e)
+    {
+        int intResult = (int) Math.pow(a,e);
+        return intResult;
+    }
+
+}
