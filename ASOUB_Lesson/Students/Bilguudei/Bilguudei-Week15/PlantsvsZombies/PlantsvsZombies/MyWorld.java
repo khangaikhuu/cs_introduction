a096562ef60ff85d5877d33ffc43becc7932c4e0
G8 <G8@asu.local>
2018-12-05 09:36:44 +0800

    Plants vs Zombies added

--- ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week15/PlantsvsZombies/PlantsvsZombies/MyWorld.java
+++ ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week15/PlantsvsZombies/PlantsvsZombies/MyWorld.java
@@ -17,5 +17,37 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
+        Plant1 plant1 = new Plant1();
+        addObject(plant1,100,400);
+        Plant5 plant5 =new Plant5();
+        addObject(plant5,100,200);
+        Plant6 plant6 = new Plant6();
+        addObject(plant6,100,70);
+        Plant2 plant2 = new Plant2();
+        addObject(plant2,200,300);
+        Plant7 plant7 = new Plant7();
+        addObject(plant7,200,500);
+        Plant3 plant3 = new Plant3();
+        addObject(plant3,600,200);
+        Plant4 plant4 = new Plant4();
+        addObject(plant4,220,70);
+        Plant9 plant9 = new Plant9();
+        addObject(plant9,500,400);
+        Zombie2 zombie2 = new Zombie2();
+        addObject(zombie2,900,300);
+        Zombie10 zombie10 = new Zombie10();
+        addObject(zombie10,1000,100);
+        Zombie3 zombie3 = new Zombie3();
+        addObject(zombie3,1000,500);
+        Zombie5 zombie5 = new Zombie5();
+        addObject(zombie5,800,400);
+        Zombie7 zombie7 = new Zombie7();
+        addObject(zombie7,700,500);
+        Zombie6 zombie6 = new Zombie6();
+        addObject(zombie6,600,50);
+        Zombie8 zombie8 = new Zombie8();
+        addObject(zombie8,700,250);
+        Owner owner = new Owner();
+        addObject(owner,30,330);
     }
 }
