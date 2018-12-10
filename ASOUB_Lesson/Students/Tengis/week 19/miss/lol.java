628a6f7165e7996ca797ed482482ab8bfa2a6abd
G8 <G8@asu.local>
2018-12-10 08:06:54 +0800

    stair

--- ASOUB_Lesson/Students/Tengis/week 19/miss/lol.java
+++ ASOUB_Lesson/Students/Tengis/week 19/miss/lol.java
@@ -1,39 +1,24 @@
 public class lol
 {
-    private String a[][];
     public void something(int n)
     {
-        a = new String[n][n];
-        int row, col;
-        for(row=n;row > 0; i--)
+        
+        for(int i = 0;i < n; i++)
         {
-            for (int j=0; j < n-i; j++)
+            for (int j = 1; j <= n; j++)
             {
-                a[i][j] = " ";
-            }
-            for(int k = n; k >= i; k--)
-            {
-                a[i][k] = "#";
-            }
-        }
-        printArray();
-    }
-    
-    public void printArray()
-    {
-        for (int i= 0; i < a.length-1; i++)
-        {
-            for (int j = 0; j < a.length; j++)
-            {
-                if (j == (a.length - 1))
+                if (j < n - i)
                 {
-                    System.out.println("");
+                    System.out.print(" ");
                 }
                 else
                 {
-                    System.out.print(a[i][j]);
+                    System.out.print("#");
                 }
             }
+            System.out.println("");
         }
+   
     }
+    
 }
