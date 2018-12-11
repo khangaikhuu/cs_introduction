1c371616aa0da9a8e4fbd88d290ca5c7976b0588
G8 <G8@asu.local>
2018-12-11 09:10:42 +0800

    added 205#

---
+++ ASOUB_Lesson/Students/Khuvituguldur/Week 15/Exercise2/Ger.java
@@ -0,0 +1,18 @@
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Ger extends Actor
+{
+    /**
+     * Act - do whatever the Ger wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(this))
+            Greenfoot.playSound("fanfare.wav");
+    }
+}
