c98af405ffe7e7ae480cde3763c60b8147c17e01
G8 <G8@asu.local>
2018-12-12 08:40:48 +0800

    hi

---
+++ ASOUB_Lesson/Students/Tengis/20 weelk/hi/rs.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rs here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rs extends Actor
+{
+    /**
+     * Act - do whatever the rs wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+         GreenfootImage image = getImage();  
+        image.scale(60, 60);      
+        setImage(image);
+        if (Greenfoot.mouseClicked(this)) {
+        Greenfoot.playSound("fuck");}
+    }    
+}
