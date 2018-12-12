2784015a9ea462138a6766b8057ccf88c9b8b568
G8 <G8@asu.local>
2018-12-12 09:45:40 +0800

    Issue #227, #228, #229

---
+++ ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week17/Tuesday/Ex1 area radius/Ex1/Ex1.java
@@ -0,0 +1,12 @@
+import java.lang.Math; 
+  
+class Ex1 { 
+  
+    // driver code 
+    public static void main(String args[]) 
+    { 
+        double a = 100; 
+  
+        System.out.println(Math.sqrt(a));
+    }
+}
---
+++ ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week17/Tuesday/Ex1 area radius/Ex1/Ex2.java
@@ -0,0 +1,12 @@
+import java.lang.Math; 
+  
+class Ex2 { 
+  
+    // driver code 
+    public static void main(String args[]) 
+    { 
+        double a = 400; 
+  
+        System.out.println(Math.sqrt(a));
+    }
+}
