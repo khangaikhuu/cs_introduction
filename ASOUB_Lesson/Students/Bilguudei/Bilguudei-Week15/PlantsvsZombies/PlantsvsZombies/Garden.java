55e5b38122d6102fed0967944148d02e7fcbdf04
G8 <G8@asu.local>
2018-12-14 08:58:21 +0800

    added #251

--- ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week15/PlantsvsZombies/PlantsvsZombies/Garden.java
+++ ASOUB_Lesson/Students/Bilguudei/Bilguudei-Week15/PlantsvsZombies/PlantsvsZombies/Garden.java
@@ -10,7 +10,9 @@ public class Garden extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-
+        
+        //Create a new object
+        
         
     }
     
