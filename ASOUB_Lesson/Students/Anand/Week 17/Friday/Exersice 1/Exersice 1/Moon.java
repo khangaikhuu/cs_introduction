738f239696ea8ef775f2566d0e03d11b2b9dbe60
G8 <G8@asu.local>
2018-12-14 08:53:38 +0800

    #245

---
+++ ASOUB_Lesson/Students/Anand/Week 17/Friday/Exersice 1/Exersice 1/Moon.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Moon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Moon extends Actor
+{
+    /**
+     * Act - do whatever the Moon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        move(4);
+        Actor a = this.getOneIntersectingObject(Earth.class);
+        if (a != null){
+            getWorld().removeObject(this);
+            Greenfoot.playSound("bang.wav");
+    }    
+}
+}
